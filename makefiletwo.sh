e415eede07efe5993faf324a09eadd0118269f87
finkegabriel <finkegabriel@gmail.com>
2019-05-13 03:21:36 -0700

    init

---
+++ makefiletwo.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+#auto dasm
+#finkegabriel
+#2019
+export DISPLAY=:0
+clear
+echo "Auto DASM/MakeWav"
+echo "================="
+echo "Input (*.asm) files are read from 'asm_input'"
+echo "Output (*.bin)"
+echo
+echo -n "What is the name of the *.asm file? "
+read input
+cd ~/code/atari/asm-atari-2600
+./dasm.Linux.x86 $input.asm -f3 -o$input.bin
+wine makewav.exe -ts -d2 -f0 $input.bin
+clear
+echo "================="
+   
+
+
+
+
+
